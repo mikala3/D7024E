7e6a1bf8eab56b87034bcc1be72fd17010f13952
Rickard <36886972+drakcir97@users.noreply.github.com>
2020-09-03 16:15:06 +0200

    Added given code & first attemt at docker file

--- helloworld.go
+++ helloworld.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func test() {
 	fmt.Println("Hello World")
 
 }
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+var channel = make(chan string)
+
+func main() {
+
+}
+
+func node() {
+	
+}
