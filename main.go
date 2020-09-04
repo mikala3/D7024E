c5198f28b67b018556d678bccde615f93a606a2b
Rickard <36886972+drakcir97@users.noreply.github.com>
2020-09-04 14:06:22 +0200

    Ooops

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import "net"
+import "log"
+import "io"
+
 func main() {
 	go dumbnode()
+}
+
+func dumbnode() {
+	go conn()
+}
+
+func conn() {
+	ln, err := net.Listen("tcp", "8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(c net.Conn) { //Connection handler
+			io.Copy(c, c)
+			c.Close()
+		}(conn)
+	}
 }
