f4712ec83f36cdea8baa9b9ef3bb49ca782c8edc
Rickard <36886972+drakcir97@users.noreply.github.com>
2020-09-04 14:07:06 +0200

    Ooops igen

--- dumbnode.go
+++ dumbnode.go
@@ -1,27 +1,27 @@
 package main
 
-import "net"
-import "log"
-import "io"
+// import "net"
+// import "log"
+// import "io"
 
-func dumbnode() {
-	go conn()
-}
+// func dumbnode() {
+// 	go conn()
+// }
 
-func conn() {
-	ln, err := net.Listen("tcp", "8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ln.Close()
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go func(c net.Conn) { //Connection handler
-			io.Copy(c, c)
-			c.Close()
-		}(conn)
-	}
-}
+// func conn() {
+// 	ln, err := net.Listen("tcp", "8080")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer ln.Close()
+// 	for {
+// 		conn, err := ln.Accept()
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		go func(c net.Conn) { //Connection handler
+// 			io.Copy(c, c)
+// 			c.Close()
+// 		}(conn)
+// 	}
+// }
