71afad2b97af7e8b8fe7e810eb02ee98ecad64ff
Rickard <36886972+drakcir97@users.noreply.github.com>
2020-10-22 20:43:42 +0200

    Update datahandler.go

--- datahandler.go
+++ datahandler.go
@@ -55,11 +55,11 @@ func (kademlia *Kademlia) DataHandler() {
 				kademlia.nt.SendFoundDataMessage(&contactarr[1],split[2],data)
 			} else {
 				data := kademlia.storage.Get(string(split[2]))
-				fmt.Println(newstring)
-				fmt.Println("Failed data: "+string(data)+ " hash: "+string(split[2]))
 				contact := NewContact(NewKademliaID(split[2]),"localhost:0000")
 				newclosest := kademlia.nt.rt.FindClosestContacts(contact.ID,1)
 				if ((!newclosest[0].ID.Equals(kademlia.nt.rt.me.ID)) || (!newclosest[0].ID.Equals(contactarr[1].ID))) { //No looping back to ourself and contact searching for data
+					fmt.Println(newstring)
+					fmt.Println("Failed data: "+string(data)+ " hash: "+string(split[2]))
 					kademlia.nt.SendLookupDataMessage(&newclosest[0],&contactarr[1],split[2])
 				}
 			}
